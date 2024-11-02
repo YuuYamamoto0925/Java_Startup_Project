be9d2b9727822236a68d64d8f05994f6a6461d91
YuuYamamoto0925 <yuusugoroku@gmail.com>
2024-11-02 15:44:03 +0900

    20241102_1543_COMMIT

---
+++ src/Sample002/Exec002.java
@@ -0,0 +1,15 @@
+package Sample002;
+public class Exec002 {
+    
+    //Javaでの演算処理
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        
+        System.out.println("足し算：" + a + "+" + b + "=" + (a+b));
+        System.out.println("引き算：" + a + "-" + b + "=" + (a-b));
+        System.out.println("かけ算：" + a + "×" + b + "=" + (a*b));
+        System.out.println("割り算：" + a + "÷" + b + "=" + (a/b));
+    }
+}
