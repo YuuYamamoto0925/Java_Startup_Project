45c46a785bc50f70dad68f14308b7ac10148cc9e
YuuYamamoto0925 <yuusugoroku@gmail.com>
2024-11-02 14:59:47 +0900

    初回COMMIT

---
+++ src/Sample001/Exec001.java
@@ -0,0 +1,8 @@
+package Sample001;
+public class Exec001 {
+    //初めてのJava
+    //Hello World!をコンソールに出力する
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
